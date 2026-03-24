dbc78927d60c11280e9fcfd43cae90db8628d53f
Kali <kali@example.com>
2026-03-24 12:11:28 +0800

    Add network resources and public IP configuration for Teamspeak VM

--- terraform/azure/data.tf
+++ terraform/azure/data.tf
@@ -2,6 +2,7 @@ data "azurerm_resource_group" "cicd" {
   name = "cicd"
 }
 
+# Reference: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/ssh_public_key
 data "azurerm_ssh_public_key" "teamspeak_ssh" {
   name                = "teamspeak-ssh"
   resource_group_name = data.azurerm_resource_group.cicd.name
--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,3 +3,45 @@ resource "azurerm_resource_group" "teamspeak" {
   location = "australiaeast"
 }
 
+# Network
+
+resource "azurerm_virtual_network" "teamspeak_vpc" {
+    name = "teamspeak-vpc"
+    location = azurerm_resource_group.teamspeak.location
+    resource_group_name = azurerm_resource_group.teamspeak.name
+    address_space = [ "10.0.0.0/8" ]
+}
+
+resource "azurerm_subnet" "teamspeak_public_subnet_1" {
+    name = "teamspeak-public-subnet-1"
+    resource_group_name = azurerm_resource_group.teamspeak.name
+    virtual_network_name = azurerm_virtual_network.teamspeak_vpc.name
+    address_prefixes = [ "10.1.0.0/16" ]
+    default_outbound_access_enabled = true
+}
+
+# IP and NIC
+
+# Provisions a static IP, similar to an AWS EIP
+resource "azurerm_public_ip" "eip" {
+    name = "teamspeak-eip"
+    location = azurerm_resource_group.teamspeak.location
+    resource_group_name = azurerm_resource_group.teamspeak.name
+    allocation_method = "Static"
+    ip_version = "IPv4"
+    sku_tier = "Regional"
+}
+
+# Network interface for the VM
+resource "azurerm_network_interface" "eni" {
+    name                = "teamspeak-eni"
+    location            = azurerm_resource_group.teamspeak.location
+    resource_group_name = azurerm_resource_group.teamspeak.name
+
+    ip_configuration {
+        name = "primary-ip_config"
+        public_ip_address_id = azurerm_public_ip.eip.id
+        subnet_id = azurerm_subnet.teamspeak_public_subnet_1.id
+        primary = true
+    }
+}
