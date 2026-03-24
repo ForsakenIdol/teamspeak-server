11ed391478cb75882506cc318784bd2f3e09436d
Kali <kali@example.com>
2026-03-24 11:34:21 +0800

    First commit: add GitHub Actions workflows for Azure Terraform deployment and planning

---
+++ terraform/azure/init.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.65.0"
+    }
+  }
+
+  cloud {
+    organization = "forsakenidol-organization-1"
+    workspaces {
+      name = "teamspeak-server"
+    }
+  }
+}
+
+provider "azurerm" {
+  # "Visual Studio Professional Subscription" in my personal tenant LDW_PERSONAL_TENANT
+  subscription_id                 = "4768df7b-f6ff-4b8b-9b0a-3c03595c7ef2"
+  resource_provider_registrations = "none"
+  features {}
+}
